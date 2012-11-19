3eb869b50b6691141b236b321eed05e259648962
Michel Rijnders <mies@tty.nl>
2012-11-19 14:34:34 +0100

    Process code review
    Commit constist of:
    - layout tweaks
    - default value for buffer field in state record
    - readystate pattern match for send

--- src/gaffer_ws_client.erl
+++ src/gaffer_ws_client.erl
@@ -3,40 +3,42 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/1]).
 -export([connect/1]).
--export([send_text/2,send_binary/2,ping/1]).
--export([frame/2]).
--export([unframe/1]).
--export([decode_len/1]).
--export([encode_len/1]).
--export([mask/2]).
+-export([send_text/2, send_binary/2]).
+-export([ping/1]).
+-export([start_link/1]).
 
 %% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
--export([key/0]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
 
 -define(DISCONNECTED, 0).
 -define(CONNECTING, 1).
 -define(OPEN, 2).
 -define(CLOSED, 3).
 
--define(OP_cont, 0).
+-define(OP_CONT, 0).
 -define(OP_TEXT, 1).
 -define(OP_BINARY, 2).
 -define(OP_CLOSE, 8).
 -define(OP_PING, 9).
 -define(OP_PONG, 10).
 
--define(SERVER, ?MODULE). 
+-define(SERVER, ?MODULE).
 
--record(state, {readystate,key,host,port,path,sock,headers,listener,buffer,
-		contop}).
+-record(state, {readystate,
+                key,
+                host, port, path,
+                sock,
+                headers,
+                listener,
+                buffer = <<>>,
+                op_cont}).
 
 %%----------------------------------------------------------------------------
 %% API
 %%----------------------------------------------------------------------------
+
 connect(Pid) ->
     gen_server:cast(Pid, {connect, self()}).
 
@@ -58,52 +60,54 @@ start_link(Url) ->
 
 init([Url]) ->
     {ok,{ws,[],Host,Port,Path,[]}} = http_uri:parse(Url),
-    {ok, #state{readystate = ?DISCONNECTED, key = key(), host = Host, 
-		port = Port, path = Path,buffer = <<>>}}.
+    {ok, #state{readystate = ?DISCONNECTED,
+                key = key(),
+                host = Host, port = Port, path = Path}}.
 
 handle_call(_Request, _From, State) ->
     {reply, ok, State}.
 
-handle_cast({connect,Pid}, State = #state{host = Host, port = Port, 
-					  path = Path, key = Key}) ->
+handle_cast({connect, Pid}, State = #state{host = Host, port = Port,
+                                           path = Path, key = Key}) ->
     {ok, Sock} = gen_tcp:connect(Host, Port, [binary, {packet, 0},
-					      {active,true}]),
+                                              {active,true}]),
     Req = initial_request(Host, Port, Path, Key),
     inet:setopts(Sock, [{packet, http}]),
     ok = gen_tcp:send(Sock, Req),
     {noreply, State#state{sock = Sock, listener = Pid}};
-handle_cast({send, Op, Data}, State = #state{sock = Sock}) ->
+handle_cast({send, Op, Data},
+            State = #state{sock = Sock, readystate = ?OPEN}) ->
     ok = gen_tcp:send(Sock, frame(Op,Data)),
     {noreply, State}.
 
-handle_info({http,_,{http_response,{1,1},101,_}}, 
-	    State = #state{readystate = ?DISCONNECTED}) ->
+handle_info({http, _, {http_response, {1, 1} , 101, _}},
+            State = #state{readystate = ?DISCONNECTED}) ->
     {noreply, State#state{readystate = ?CONNECTING}};
-handle_info({http,_,{http_header,_,Name,_,Value}}, 
-	    State = #state{readystate = ?CONNECTING}) ->
-    H = [{Name, Value} | State#state.headers],
-    {noreply, State#state{headers=H}};
-handle_info({http,_,http_eoh}, State = #state{readystate = ?CONNECTING}) ->
+handle_info({http, _, {http_header, _, Name, _, Value}},
+            State = #state{readystate = ?CONNECTING, headers = Hs}) ->
+    {noreply, State#state{headers = [{Name, Value} | Hs]}};
+handle_info({http, _, http_eoh}, State = #state{readystate = ?CONNECTING}) ->
     {noreply, handshake(State)};
-handle_info({tcp,_,Frame}, State = #state{readystate = ?OPEN, sock = Sock,
-					  contop = Cop,
-					  listener = Listener,
-					  buffer = Buffer}) ->
+handle_info({tcp,_,Frame}, State = #state{readystate = ?OPEN,
+                                          sock = Sock,
+                                          op_cont = Cop,
+                                          listener = Listener,
+                                          buffer = Buffer}) ->
     New_state = case unframe(Frame) of
-		    {Op, Data, 0} ->
-			State#state{buffer = <<Buffer/binary, Data/binary>>, 
-				    contop = cont_op(Cop, Op)}; 
-		    {Op, Data, 1} -> 
-                        Opcode = cont_op(Cop, Op),
-			Listener ! {Opcode, <<Buffer/binary, Data/binary>>},
-			handle_control(Opcode, Sock),
-			State#state{buffer = <<>>} 
-		end,
+                    {Op, Data, 0} ->
+                        State#state{buffer = <<Buffer/binary, Data/binary>>,
+                                    op_cont = op_cont(Cop, Op)};
+                    {Op, Data, 1} ->
+                        Opcode = op_cont(Cop, Op),
+                        Listener ! {Opcode, <<Buffer/binary, Data/binary>>},
+                        handle_control(Opcode, Sock),
+                        State#state{buffer = <<>>}
+                end,
     {noreply, New_state};
-handle_info({tcp_closed, _Socket},State) ->
-    {stop,normal,State#state{readystate = ?CLOSED}};
+handle_info({tcp_closed, _Socket}, State) ->
+    {stop, normal, State#state{readystate = ?CLOSED}};
 handle_info({tcp_error, _Socket, _Reason},State) ->
-    {stop,tcp_error,State#state{readystate = ?CLOSED}};
+    {stop, tcp_error, State#state{readystate = ?CLOSED}};
 handle_info(_, State) ->
     {noreply, State}.
 
@@ -113,10 +117,10 @@ terminate(_Reason, _State) ->
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
+%%----------------------------------------------------------------------------
+%% Internal functions
+%%----------------------------------------------------------------------------
 
-%%----------------------------------------------------------------------------
-%% Internal functions 
-%%----------------------------------------------------------------------------
 handle_control(close, Sock) ->
     gen_tcp:close(Sock);
 handle_control(ping, Sock) ->
@@ -124,9 +128,9 @@ handle_control(ping, Sock) ->
 handle_control(_,_) ->
     ok.
 
-cont_op(Cop, ?OP_cont) ->
+op_cont(Cop, ?OP_CONT) ->
     Cop;
-cont_op(_, Op) ->
+op_cont(_, Op) ->
     Op.
 
 key() ->
@@ -139,25 +143,25 @@ initial_request(Host,Port, Path, Key) ->
     "GET "++ Path ++" HTTP/1.1\r\n" ++
     "Host: " ++ Host ++ ":" ++ integer_to_list(Port) ++ "\r\n" ++
     "Connection: Upgrade\r\n" ++
-    "Upgrade: websocket\r\n" ++ 
+    "Upgrade: websocket\r\n" ++
     "Sec-WebSocket-Version: 13\r\n" ++
     "Sec-WebSocket-Key: " ++ Key ++ "\r\n\r\n".
 
-handshake(State = #state{readystate = ?CONNECTING, sock = Sock, key = Key, 
-			 headers = Headers}) ->
+handshake(State = #state{readystate = ?CONNECTING, sock = Sock, key = Key,
+                         headers = Headers}) ->
     "upgrade" = string:to_lower(proplists:get_value('Connection', Headers)),
     "websocket" = string:to_lower(proplists:get_value('Upgrade', Headers)),
     Accept = proplists:get_value("Sec-Websocket-Accept", Headers),
     MagicString = "258EAFA5-E914-47DA-95CA-C5AB0DC85B11",
     Expected = binary_to_list(base64:encode(crypto:sha(Key ++ MagicString))),
-    case Accept =:= Expected of 
-        true -> 
+    case Accept =:= Expected of
+        true ->
             inet:setopts(Sock, [{packet, raw}]),
             State#state{readystate = ?OPEN};
-        _ -> 
+        _ ->
             State#state{readystate = ?CLOSED}
     end.
-    
+
 mask(<<>>, _) ->
     <<>>;
 mask(<<D:24>>, <<M:24, _:8>>) ->
@@ -177,10 +181,10 @@ unmask(1, << Mask:32, Data/bits >> ) ->
     mask(Data, <<Mask:32>>).
 
 op_to_atom(?OP_TEXT) -> text;
-op_to_atom(?OP_BINARY) -> binary; 
-op_to_atom(?OP_PING) -> ping; 
-op_to_atom(?OP_PONG) -> pong; 
-op_to_atom(?OP_CLOSE) -> close. 
+op_to_atom(?OP_BINARY) -> binary;
+op_to_atom(?OP_PING) -> ping;
+op_to_atom(?OP_PONG) -> pong;
+op_to_atom(?OP_CLOSE) -> close.
 
 unframe(<< F:1, _:3, Op:4, M:1, Data/bits >>) ->
     {Len, Rest} = decode_len(Data),
@@ -202,7 +206,7 @@ encode_len(Len) when Len < 126 ->
 encode_len(Len) when Len =< 65535 ->
     << 126:7, Len:16 >>;
 encode_len(Len) when Len =< 18446744073709551615 ->
-    << 127:7, Len:64 >>.    
+    << 127:7, Len:64 >>.
 
 decode_len(<<127:7, Len:64, Rest/bytes>>) ->
     {Len, Rest};
